c589d368b5c9278b06d074fe5a03648dbf1f1f69
Tobias Klug <tbs.klg@gmail.com>
2025-01-30 16:32:02 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use std::io::{self, Write};
 
 fn main() {
     let stdin = io::stdin();
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        match input.trim() {
+            "exit" => break,
+            _ => println!("{}: command not found", input.trim()),
+        }
     }
 }
